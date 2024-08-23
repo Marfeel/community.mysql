8627ab1a97b4afbf8b764fc575bb970f78aee482
Fran <51233345+francescsanjuanmrf@users.noreply.github.com>
2024-08-23 17:07:02 +0200

    Update plugins/module_utils/user.py
    Co-authored-by: Andrew Klychkov <aaklychkov@mail.ru>

--- plugins/module_utils/user.py
+++ plugins/module_utils/user.py
@@ -393,10 +393,8 @@ def user_mod(cursor, user, host, host_all, password, encrypted,
                 update = True
 
             if update:
-                password_changed = True
-                msg = "Auth pugin updated"
                 if module.check_mode:
-                    return {'changed': True, 'msg': msg, 'password_changed': password_changed}
+                    return {'changed': True, 'msg': 'Auth plugin updated', 'password_changed': True}
                 if plugin_hash_string:
                     query_with_args = "ALTER USER %s@%s IDENTIFIED WITH %s AS %s", (user, host, plugin, plugin_hash_string)
                 elif plugin_auth_string:
